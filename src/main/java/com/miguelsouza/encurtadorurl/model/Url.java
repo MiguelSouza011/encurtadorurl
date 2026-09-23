d56fbb6a0552b465c7891e50efd0c41e917c7f2e
Miguel Souza <souzzamiguel09@gmail.com>
2026-09-22 22:08:56 -0300

    Created Url model

---
+++ src/main/java/com/miguelsouza/encurtadorurl/model/Url.java
@@ -0,0 +1,41 @@
+package com.miguelsouza.encurtadorurl.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalUrl;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+}
+}
